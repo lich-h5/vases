c3b6a98257f182c091cbe20b7238fb243fecf523
JDZChao <jdzchao@gmail.com>
2020-06-12 18:25:46 +0800

    解决重复加载问题

--- utils/image.js
+++ utils/image.js
@@ -1,12 +1,16 @@
 export function load(file, callback) {
     const img = document.createElement("img");
+    let status = false;
     file = file.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
+
     reader.onload = function (e) {
         img.src = this.result;
-        img.onload = function () {
-            (callback && callback(img))
-        }
+    }
+    img.onload = function (e) {
+        if (status) return
+        status = true
+        callback(img)
     }
 }
